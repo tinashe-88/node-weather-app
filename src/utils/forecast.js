f0449242d6c832fa3372d2b346c68370f92af476
Tinashe <tinashejchiweshe@gmail.com>
2019-10-16 16:54:16 +0200

    added temp high and low to forecast

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -9,7 +9,13 @@ const forecast = (longitude, latitude, callback) => {
         } else if(body.error){
             callback('Whoops! Unable to find location. Try again..');
         } else {
-            callback(undefined, ` ${body.daily.data[0].summary} It is currently ${body.currently.temperature}°C. There's a ${body.currently.temperature}% chance of rain.`);
+            
+            callback(undefined, `
+                ${body.daily.data[0].summary} 
+                It is currently ${body.currently.temperature}°C. 
+                The high today is ${body.daily.data[0].temperatureHigh} with a low of ${body.daily.data[0].temperatureLow}. 
+                There's a ${body.currently.temperature}% chance of rain.
+                `);
         }
     })
 };
